50e23f5c03fd4bc11007d88ee9b9051483881ac9
STARS1LVER <diegocardenaz11@gmail.com>
2024-03-15 15:43:07 -0500

    Adding routes

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,21 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { mergeApplicationConfig, ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { provideRouter, withViewTransitions } from '@angular/router';
+import { routes } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
 
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering()
-  ]
+  providers: [provideRouter(
+    routes,
+    withViewTransitions({
+      skipInitialTransition: true // ? que opmita la primera trasnsaccion
+    })
+  ),
+  importProvidersFrom(
+    HttpClientModule
+  )
+]
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,34 @@ export const routes: Routes = [
 
   {
     path:'dashboard',
-    loadComponent: () => import('./dashboard/dashboard.component')
+    loadComponent: () => import('./dashboard/dashboard.component'),
+    children: [
+      {
+        path: 'list-pokemon',
+        title: 'List Pokemon',
+        loadComponent: () => import('./dashboard/pages/list-pokemon/list-pokemon.component')
+      },
+      {
+        path:  'search-pokemon',
+        title: 'Search Pokemon',
+        loadComponent: () => import('./dashboard/pages/search-pokemon/search-pokemon.component')
+      },
+      {
+        path: 'info-pokemon/:id',
+        title: 'Title Info Pokemon',
+        loadComponent: () => import('./dashboard/pages/info-pokemon/info-pokemon.component')
+      },
+      {
+        path: '',
+        redirectTo: 'list-pokemon',
+        pathMatch: 'full'
+      }
+    ],
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
   }
 
 
