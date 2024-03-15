7eff2eaf224521f79fb8c811ff60539afe47e7ba
STARS1LVER <diegocardenaz11@gmail.com>
2024-03-15 15:20:42 -0500

    Adding to repository

---
+++ src/app/dashboard/pages/info-pokemon/info-pokemon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-pokemon',
+  standalone: true,
+  imports: [],
+  templateUrl: './info-pokemon.component.html',
+  styleUrl: './info-pokemon.component.css'
+})
+export default class InfoPokemonComponent {
+
+}
